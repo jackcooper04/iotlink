15bba226ae6b01b7a1364ad8621dc227893751e3
Jack Cooper <jack@acooper.net>
2021-01-17 14:55:55 +0000

    V1.0

--- iotlink.js
+++ iotlink.js
@@ -2,8 +2,11 @@ var mqtt = require("mqtt");
 const yaml = require('js-yaml')
 const fs = require('fs');
 const os = require('os');
+const path = require('path')
 const deviceName = os.hostname().toLowerCase();
-var fileContents = fs.readFileSync('C:\\ProgramData\\IOTLink\\Configs\\configuration.yaml','utf-8')
+const uri = 'iotlink/workgroup/'+deviceName
+const fileLocation = path.join(process.env.PROGRAMDATA,'IOTLink\\Configs\\configuration.yaml');
+var fileContents = fs.readFileSync(fileLocation,'utf-8')
 var data = yaml.load(fileContents);
 var options = {
    "port": data.mqtt.tcp.port,
@@ -22,16 +25,66 @@ var options = {
 //var options = require('./mqtt-options.json');
 var client = mqtt.connect(options.host,options);
 
-client.subscribe("iotlink/workgroup/jackspc/windows-monitor/#",function(){
-    client.on('message',function(topic,message,packet){
-        //console.log(packet)
-    })
-})
 
-function showNotification(title,message){
-    var data = {
-        title:title,
-        message:message
-    };
-    client.publish('iotlink/workgroup/'+deviceName+'/commands/notify',JSON.stringify(data));
+
+module.exports = {
+    sendNotification:function(title,message){
+        var data = {
+            title:title,
+            message:message
+        };
+        client.publish(uri+'/commands/notify',JSON.stringify(data));
+    },
+    lockComputer:function(){
+        client.publish(uri+'/commands/lock');
+    },
+    shutdownComputer:function(){
+        client.publish(uri+'/commands/shutdown');
+    },
+    rebootComputer:function(){
+        client.publish(uri+'/commands/reboot');
+    },
+    hibernateComputer:function(){
+        client.publish(uri+'/commands/hibernate');
+    },
+    logOffComputer:function(){
+        client.publish(uri+'/commands/logoff');
+    },
+    toggleMedia:function(){
+        client.publish(uri+'/commands/media/playpause');
+    },
+    stopMedia:function(){
+        client.publish(uri+'/commands/media/stop');
+    },
+    nextSong:function(){
+        client.publish(uri+'/commands/media/next');
+    },
+    previousSong:function(){
+        client.publish(uri+'/commands/media/previous');
+    },
+    toggleDisplay:function(state){
+        if (state == "on" || state == "off"){
+            client.publish(uri+'/commands/displays/'+state);
+        }
+        
+    },
+    openApplication:function(path){
+        var pathArray = path.split("\\");
+        pathArray.pop();
+        var pathString = pathArray.join("\\")
+        var body = {
+            command:path,
+            args:"",
+            path:pathString,
+            user:"",
+            visible:true,
+            fallback:true
+        };
+        client.publish(uri+'/commands/run',JSON.stringify(body));
+    }
 }
+
+
+
+
+
